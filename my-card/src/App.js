6b9491279e55102fb76c1cfa8d493dd07e1cba12
DiSantoz <dferreira0191@gmail.com>
2023-10-11 11:10:58 -0400

    create react app and file structure

---
+++ my-card/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {}
+
+function Intro() {}
+
+function SkillList() {}
+
+export default App;
